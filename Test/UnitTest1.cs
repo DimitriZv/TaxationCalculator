2744405dfc7d1e5db0d00278c797d85f54cf0ed8
samuel micallef <sammy.m1285@gmail.com>
2021-05-10 20:58:33 +1000

    non resident 2017-2018 all tax bracket tests

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -181,7 +181,7 @@ namespace Test       //"2019-20"   "2018-19"   "2017-18"
             string inputAmount = "";
             string inputYear = "2017-18";
             bool status = true;
-            string expectResult = "Error – Please enter a value";
+            string expectResult = "Error: Please input an amount.";
             string actualResult;
 
             //step 2 - Act
@@ -196,7 +196,7 @@ namespace Test       //"2019-20"   "2018-19"   "2017-18"
             string inputAmount = " ";
             string inputYear = "2017-18";
             bool status = true;
-            string expectResult = "Error – Please enter a value";
+            string expectResult = "Error: Please input an amount.";
             string actualResult;
 
             //step 2 - Act
@@ -218,6 +218,89 @@ namespace Test       //"2019-20"   "2018-19"   "2017-18"
             actualResult = Calculate.Calculating(inputAmount, inputYear, status);
 
             //step 3 - Assert
+            Assert.AreEqual(expectResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod15()    //2017-2018 non-resident 0-87,000 bracket    *****this uses random input so it can be called in a loop to generate tests (NOT YET IMPLEMENTED)  
+        {
+            Random rnd = new Random();
+            int randInputAmount = rnd.Next(0, 87001);  //create number between 0 and 87000 (not inclusive)
+
+            string inputAmount = randInputAmount.ToString();  //convert to string, as calculationg method only accepts string
+
+            string inputYear = "2017-18";
+            bool status = false;    //residency status
+
+            double taxPayable = randInputAmount * 0.325;    //calculating the amount of tax payable
+            double afterTaxIncome = randInputAmount - taxPayable;
+
+            string taxPayableString = Calculate.currenceView(taxPayable);
+            string afterTaxIncomeString = Calculate.currenceView(afterTaxIncome);
+
+            string expectResult = "The estimated tax on your taxable income is $" + taxPayableString + "\nYour estimated income after tax is $" + afterTaxIncomeString;
+            string actualResult;
+
+            //generate actual resluts for comparison
+            actualResult = Calculate.Calculating(inputAmount, inputYear, status);
+
+            //compare expected vs actual results
+            Assert.AreEqual(expectResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void TestMethod16()    //2017-2018 non-resident 87001-180000 bracket    *****this uses random input so it can be called in a loop to generate tests (NOT YET IMPLEMENTED)  
+        {
+            Random rnd = new Random();
+            int randInputAmount = rnd.Next(87001, 180001);  //create number between 87001 and 180000 (not inclusive)
+
+            string inputAmount = randInputAmount.ToString();  //convert to string, as calculationg method only accepts string
+
+            string inputYear = "2017-18";
+            bool status = false;    //residency status
+
+            double taxPayable = ((randInputAmount -87000)* 0.37) + 28275;    //calculating the amount of tax payable
+            double afterTaxIncome = randInputAmount - taxPayable;
+
+            string taxPayableString = Calculate.currenceView(taxPayable);
+            string afterTaxIncomeString = Calculate.currenceView(afterTaxIncome);
+
+            string expectResult = "The estimated tax on your taxable income is $" + taxPayableString + "\nYour estimated income after tax is $" + afterTaxIncomeString;
+            string actualResult;
+
+            //generate actual resluts for comparison
+            actualResult = Calculate.Calculating(inputAmount, inputYear, status);
+
+            //compare expected vs actual results
+            Assert.AreEqual(expectResult, actualResult);
+        }
+
+
+        [TestMethod]             
+        public void TestMethod17()    //2017-2018 non-resident 180001 and up bracket  *****this uses random input so it can be called in a loop to generate tests (NOT YET IMPLEMENTED)   
+        {
+            Random rnd = new Random();
+            int randInputAmount = rnd.Next(180001, 100000001);  //create number between 180,001 and 100,000,000 (not inclusive) i just chose 100mil as i didnt want to go to high
+
+            string inputAmount = randInputAmount.ToString();  //convert to string, as calculationg method only accepts string
+
+            string inputYear = "2017-18";
+            bool status = false;    //residency status
+
+            double taxPayable = ((randInputAmount - 180000) * 0.45) + 62685;    //calculating the amount of tax payable
+            double afterTaxIncome = randInputAmount - taxPayable;
+
+            string taxPayableString = Calculate.currenceView(taxPayable);
+            string afterTaxIncomeString = Calculate.currenceView(afterTaxIncome);
+
+            string expectResult = "The estimated tax on your taxable income is $" + taxPayableString + "\nYour estimated income after tax is $" + afterTaxIncomeString;
+            string actualResult;
+
+            //generate actual resluts for comparison
+            actualResult = Calculate.Calculating(inputAmount, inputYear, status);
+
+            //compare expected vs actual results
             Assert.AreEqual(expectResult, actualResult);
         }
     }
