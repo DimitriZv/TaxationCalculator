a00bcd162115fb94c142634ce7496fc904f43a78
Linus Knowlson <71065813+LinusKnowlson@users.noreply.github.com>
2021-05-17 19:19:19 +1000

    Update Program.cs
    Edit Program.cs

--- TaxCalculator/Program.cs
+++ TaxCalculator/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Library;
 
 namespace TaxCalculator
 {
@@ -14,9 +15,21 @@ namespace TaxCalculator
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //start the testing before running the tax calculator
+            UnitTestResident1718 unitTest = new UnitTestResident1718();
+            double rate = unitTest.TestMethod();
+            if (rate < 1)
+            {
+                Console.WriteLine("Fail the test");
+            }
+            else
+            {
+                Console.WriteLine("Pass the test");
+                //if pass the test, then the tax calculator will be able to run
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
